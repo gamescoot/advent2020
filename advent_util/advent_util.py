323f05dc465b60d0f93f7a675117628b8e9cdc91
gamescoot <scott@heyharris.com>
2020-12-05 19:46:42 -0500

    read_file now strips trailing blank characters or lines.

--- advent_util/advent_util.py
+++ advent_util/advent_util.py
@@ -4,7 +4,7 @@ READWRITE_MODE = 'r'
 # Reads file with given filename and returns the entire text of the file
 def read_file(filename='input'):
     file = open(filename, READWRITE_MODE)
-    file_text = file.read()
+    file_text = file.read().rstrip()
     file.close()
     return file_text
 
